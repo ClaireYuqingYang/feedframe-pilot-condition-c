956b597eda4e228b557c917d66721998a1f5fed2
ClaireYuqingYang <yy5550@nyu.edu>
2026-06-23 22:38:23 -0400

    Shorten feed timing thresholds

--- DICE/static/js/feed_timer.js
+++ DICE/static/js/feed_timer.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
         return;
     }
 
-    const showButtonAfterMs = 1 * 60 * 1000;
-    const autoSubmitAfterMs = 3 * 60 * 1000;
+    const showButtonAfterMs = 30 * 1000;
+    const autoSubmitAfterMs = 90 * 1000;
 
     setTimeout(function () {
         submitButton.style.display = '';
